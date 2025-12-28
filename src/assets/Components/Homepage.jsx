e95fddd65fac6432ef1b6b36168561073aee8a47
0xhakilKE <shakilochieng@gmail.com>
2025-12-28 02:55:19 -0800

    launching the template

---
+++ src/assets/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Hello Kenyan People</h1>
+            <p>Describing the Kenyan people is one heck of an offensive disguise that makes life so toleretable to the <br />
+                masses</p>
+        </div>
+    )
+}
+
+export default Homepage

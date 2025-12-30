ff94ee15da631c102af2802ad9785b86de20fbf2
0xhakilKE <shakilochieng@gmail.com>
2025-12-30 11:06:00 -0800

    little edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Homepage from './assets/Components/Homepage'
 
 const App = () => {
+
+  let name = "Shakil Omondi"
+  let school = "Bwanda Primary School"
   return (
     <div>
 
+      <h1>{name} is a student at {school}</h1>
     </div>
   )
 }

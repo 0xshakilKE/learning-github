09ab5321ce6e1dbf056d961c33b2550873301598
0xhakilKE <shakilochieng@gmail.com>
2026-01-01 02:30:46 -0800

    Small Edits

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
     <div>
 
       <h1>{name} is a student at {school}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere quos dolores quo reiciendis qui! Vero dolorum modi ea. Inventore, rem eum expedita dolore explicabo exercitationem est reiciendis ducimus, eius tempora adipisci quod at ratione id veniam sunt ea illo sint odio corporis modi voluptatem? Accusantium quidem libero quam porro alias cupiditate similique doloribus, laudantium asperiores nihil dolores eaque eos ut ipsa atque magni possimus itaque! Illo, exercitationem.</p>
     </div>
   )
 }

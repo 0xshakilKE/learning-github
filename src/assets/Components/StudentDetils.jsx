c8ea32b3555d2a200874d84a5f47597c427fd56f
0xhakilKE <shakilochieng@gmail.com>
2026-01-02 14:47:59 +0300

    New Student Details

---
+++ src/assets/Components/StudentDetils.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentDetils = () => {
+
+    let studentClass = "F4 North"
+    return (
+        <div>StudentDetils</div>
+    )
+}
+
+export default StudentDetils
